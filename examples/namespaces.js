bef1c523a3e39e8e4d6b6c1fd60c450e07aa716e
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-07 04:53:38 -0400

    improve namespaces example,
    create an app instance for each namespace

--- examples/namespaces.js
+++ examples/namespaces.js
@@ -1,35 +1,44 @@
+var App = require('./app').App;
+var app = new App();
+var one = new App();
+var two = new App();
+
 var visit = require('collection-visit');
 var minimist = require('minimist');
 var methods = require('..');
-
-var app = {
-  data: {},
-  get: function (key) {
-    return app.data[key];
-  },
-  set: function (key, value) {
-    if (typeof key === 'object') {
-      visit(app, 'set', key);
-    } else {
-      app.data[key] = value;
-    }
-    return app;
-  }
-};
 
 var cli = require('minimist-plugins')(minimist)
   .use(require('minimist-expand'))
   .use(require('minimist-events'))
   .use(methods(app))
-  .use(methods('one', app))
-  .use(methods('two', app))
+  .use(methods('one', one))
+  .use(methods('two', two))
 
+// set
+cli.on('set', console.log.bind(console, '[set]'));
+cli.one.on('set', console.log.bind(console, '[one.set]'));
+cli.two.on('set', console.log.bind(console, '[two.set]'));
 
+// get
 cli.on('get', console.log.bind(console, '[get]'));
-cli.one.on('set', console.log.bind(console, '[set]'));
-cli.one.on('get', console.log.bind(console, '[get]'));
+cli.one.on('get', console.log.bind(console, '[one.get]'));
+cli.two.on('get', console.log.bind(console, '[two.get]'));
 
+cli.on('end', function () {
+  console.log(app.cache)
+});
 
 var args = process.argv.slice(2);
-var argv = cli(args.length ? args : ['--one.set=a:b', '--one.set=c:d', '--get=a']);
+var argv = cli(args.length ? args : [
+  '--set=w:x,y,z',
+  '--one.set=a:b|c:d|e:f',
+  '--two.set=g.h.i.j:k',
+  '--get=w',
+  '--one.get=a',
+  '--two.get=g',
+]);
+
 console.log(argv);
+console.log(app);
+console.log(one);
+console.log(two);
